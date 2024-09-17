147d76fe3e2e7a0996832e279f99138926f27481
bruninrko123 <brnin9034@gmail.com>
2024-09-17 18:34:09 -0300

    project changed to check grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,74 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+    {   
+        // declaring the letter variable
+        string letter = "";
+
+        // Get the grade percentage from the user
+        Console.Write("Please enter your grade percentage: ");
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+
+        
+        // decide which letter the grade means
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+
+        else 
+        {
+            letter = "F";
+        }
+
+        // add the + or - or nothing to the letter
+        int signTester = gradePercentage % 10;
+        if (signTester >= 7)
+        {
+            letter = letter + "+";
+        }
+
+        else if (signTester <= 3)
+        {
+            letter = letter + "-";
+        }
+        else
+        {
+
+        }
+
+
+        //print the letter
+        Console.WriteLine($"Your grade is: {letter}");
+
+
+
+        // check wheter the student passed the class
+        Console.WriteLine();
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you didn't pass this class. Keep trying and trust the lord. You can do it again and succeed");
+        }
     }
-}
+};
