ca08a0657ef0344ffd7f1870a9d6ebc178106d32
bruninrko123 <brnin9034@gmail.com>
2024-09-18 20:34:22 -0300

    program 4 made

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -3,7 +3,53 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    {   // declaring variables
+        int number;
+        int sumKeeper = 0;
+        int helper = 0;
+        long smallestPositiveHelper = 999999;
+        List<int> numbers = new List<int>();
+        // getting a list of numbers from the user 
+        Console.WriteLine("Enter a list of numbers. Type 0 whe you are done");
+        do{
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while(number != 0);
+
+        //get the sum, average and the largest number of the list
+
+        foreach (int x in numbers)
+        {   
+            // get the largest number
+            if (x > helper)
+            {
+               helper = x;
+            }
+
+            else if (x > 0 && x < smallestPositiveHelper)
+            {
+                smallestPositiveHelper = x;
+            }
+            // sum numbers
+            sumKeeper = sumKeeper + x;
+
+        }
+        Console.WriteLine();
+        Console.WriteLine($"The sum of the numbers is: {sumKeeper}");
+        Console.WriteLine($"The average is {sumKeeper / numbers.Count}");
+        Console.WriteLine($"The largest number is {helper}");
+        Console.WriteLine($"The smallest positive number is {smallestPositiveHelper}");
+        Console.Write("The sorted list is: ");
+        numbers.Sort();
+        foreach (int piece in numbers)
+        {   
+            
+            Console.Write($"{piece}, ");
+        }
+
     }
 }
