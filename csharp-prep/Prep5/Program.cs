4627b4a16a6fd8c486bb77d113f2a6ca9f2c2756
bruninrko123 <brnin9034@gmail.com>
2024-09-19 11:35:22 -0300

    all programs made

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,54 @@ using System;
 
 class Program
 {
+        
+        //declaring functions 
+       static void DisplayWelcome()
+       {
+        Console.WriteLine("Welcome to the Program!");
+       }
+
+
+       static string PromptUserName()
+       {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        return name;
+       }
+
+
+       static int PromptUserNumber()
+       {
+        Console.Write("Type your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+
+       }
+
+
+       static int SquareNumber(int NumberToBeSquared)
+       {
+        int square = NumberToBeSquared * NumberToBeSquared;
+        return square;
+       }
+
+
+       static void DisplayResult(string UserName, int SquaredNumber)
+       {
+        Console.WriteLine($"The User's name is: {UserName}");
+        Console.WriteLine($"The squared number is: {SquaredNumber}");
+       }
+
+
+
+
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+    {   
+        DisplayWelcome();
+        string Nome = PromptUserName();
+        int Numero = PromptUserNumber();
+        int Square = SquareNumber(Numero);
+        DisplayResult(Nome, Square);
+
     }
 }
