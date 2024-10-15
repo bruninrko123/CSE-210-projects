bd279080cf0ff8433d69f59df2edee9600177d2a
bruninrko123 <brnin9034@gmail.com>
2024-10-14 22:47:44 -0300

    learning activity done

---
+++ prepare/Learning05/MathAssignment.cs
@@ -0,0 +1,19 @@
+public class MathAssignment : Assignment
+{
+    private string _textBookSection;
+    private string _problems;
+
+    public MathAssignment(string textBookSection, string problems, string studentName, string topic) : base(studentName, topic)
+    {
+        _textBookSection = textBookSection;
+        _problems = problems;
+    }
+
+
+    public string GetHomeworkList()
+    {
+        return $"{GetSummary()} \nSection: {_textBookSection} - Problems: {_problems}";
+    }
+
+    
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment = new Assignment("Bruno", "Programming");
+        Console.WriteLine(assignment.GetSummary());
+
+        
+        MathAssignment mathAssignment = new MathAssignment("21", "10-22", "Thamara", "Marketing");
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+
+        WritingAssignment writingAssignment = new WritingAssignment("'What can we do about it?'","Marco", "Poverty in Africa");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
