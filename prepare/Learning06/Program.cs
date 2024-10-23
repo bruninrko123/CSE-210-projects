83a0c920c3b6a397f49c3039bc951ea7fef87b97
bruninrko123 <brnin9034@gmail.com>
2024-10-23 10:52:55 -0300

    learning activity done

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning06 World!");
+
+        Square square = new Square("blue", 4);
+        
+
+
+        Circle circle = new Circle("red", 5.65);
+        
+
+        Rectangle rectangle = new Rectangle("pink", 12, 2);
+       
+
+        List<Shape> shapes = [square, circle, rectangle];
+
+        foreach (Shape s in shapes)
+        {   
+            Console.WriteLine();
+            Console.WriteLine($"Color: {s.GetColor()}");
+            Console.WriteLine($"Area: {s.GetArea()}");
+        }
+
     }
 }
