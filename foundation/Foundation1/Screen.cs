8306271e073a2cab8e6c409e4ab1c0b94c7e275d
bruninrko123 <u372026907@byui.edu>
2024-11-01 21:44:46 -0300

    testing

--- foundation/Foundation1/Screen.cs
+++ foundation/Foundation1/Screen.cs
@@ -7,7 +7,8 @@ public class Screen
 public void DisplayScreen()
 {
     foreach (Video v in _videos)
-    {
+    {   
+        
         Console.WriteLine(v.VideoInfo());
         Console.WriteLine();
         foreach (Comment c in v._comments)
